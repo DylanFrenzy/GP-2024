bc9138cfd0930f6bf2c383c68ad34062b13246b9
dylan <C24486954@myTUDublin.ie>
2024-10-07 16:47:12 +0100

    7/10/2024 work

---
+++ the-course/character_body_2d.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var direction = Input.get_vector("turn_left","turn_right","move_forwards","move_backwards")
+	velocity = direction * 50000
+	move_and_slide()
